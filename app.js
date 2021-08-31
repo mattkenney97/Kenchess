c3bfa84aed67fa60378c0c7e1950f7686310b800
Matthew Kenney <matthewkenney@Matthews-MacBook-Pro-2.local>
2021-08-31 09:08:06 -0400

    Piece movements done

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// let todoList = [];
-// let input = prompt("Todo list action");
-
-// while (input.toLowerCase() != 'quit') {
-//     console.log(input)
-
-//     if (input.toLowerCase() === 'add') {
-//         todoList.push(prompt("What do you want to add?"))
-//     }
-//     else if (input.toLowerCase() === 'list') {
-//         console.log(todoList)
-//     }
-//     else if (input.toLowerCase() === 'delete') {
-//         let remove = prompt("What do you want to delete?");
-
-//     }
-//     input = prompt("Todo list action");
-// }
-
 
 class chessBoard {
     constructor() {
@@ -39,6 +20,7 @@ class chessBoard {
             'pawn': '\u2659',
         };
         this.cols = ['a','b','c','d','e','f','g','h'];
+        this.enPassant = false;
 
         this.createBoardButtons()
     }
@@ -56,8 +38,28 @@ class chessBoard {
                             if (check.innerHTML === board.whitePieces['pawn']) {
                                 validMove = board.validWhitePawnMove(check, button)
                             }
-                            else if (check.innerHTML === board.whitePieces['rook']) {
-                                validMove = board.validWhiteRookMove(check, button)
+                            else if (check.innerHTML === board.blackPieces['pawn']) {
+                                validMove = board.validBlackPawnMove(check, button)
+                            }
+                            else if (check.innerHTML === board.whitePieces['rook'] || check.innerHTML === board.blackPieces['rook']) {
+                                validMove = board.validRookMove(check, button)
+                                board.enPassant = false
+                            }
+                            else if (check.innerHTML === board.whitePieces['bishop'] || check.innerHTML === board.blackPieces['bishop']) {
+                                validMove = board.validBishopMove(check, button)
+                                board.enPassant = false
+                            }
+                            else if (check.innerHTML === board.whitePieces['knight'] || check.innerHTML === board.blackPieces['knight']) {
+                                validMove = board.validKnightMove(check, button)
+                                board.enPassant = false
+                            }
+                            else if (check.innerHTML === board.whitePieces['king'] || check.innerHTML === board.blackPieces['king']) {
+                                validMove = board.validKingMove(check, button)
+                                board.enPassant = false
+                            }
+                            else if (check.innerHTML === board.whitePieces['queen'] || check.innerHTML === board.blackPieces['queen']) {
+                                validMove = board.validQueenMove(check, button)
+                                board.enPassant = false
                             }
                             if(validMove) {
                                 button.innerHTML = check.innerHTML
@@ -78,93 +80,383 @@ class chessBoard {
 
     validWhitePawnMove(start, end) {
         console.log('Trying pawn move')
-        if(start.name === end.name) {
+        if(!this.checkStartAndEnd(start,end)) {
             console.log('Invalid pawn move')
             return false
         }
-        let ind = 0;
-        for( let letter of this.cols ) {
-            if ( letter === start.name[0] ) {
-                break;
-            }
-            ind += 1
-        }
+        
+        let ind = this.getColumnIndex(start);
         let front2 = null;
     
         if( start.name[1] === '2' ) {
             front2 = this.cols[ind] + (parseInt(start.name[1]) + 2)
         }
+        if (end.name != front2) {
+            front2 = null
+        }
     
-        let front = this.cols[ind] + (parseInt(start.name[1])+1)
-    
+        let front = this.cols[ind] + (parseInt(start.name[1])+1);
+        
+        let frontButton = document.querySelector(`button[name="${front}"]`);
+        if(frontButton.innerHTML != "") {
+            front2 = null
+            front = null
+        }
+
         if (end.name != front) {
             front = null
         }
-        else {
-            if(end.innerHTML != "") {
-                front= null
-            }
-        }
-        let leftFront = null;
-        let rightFront = null;
-        if (start.name[0] != 'a') {
-            if (Object.values(this.blackPieces).includes(end.innerHTML)) {
-                leftFront = this.cols[ind - 1] + (parseInt(start.name[1]) + 1)
-            }
-            if (end.name != leftFront) {
-                leftFront = null
-            }
-        }
-        if (start.name[0] != 'h') {
-            if (Object.values(this.blackPieces).includes(end.innerHTML)) {
-                rightFront = this.cols[ind + 1] + (parseInt(start.name[1]) + 1)
-            }
-            if (end.name != rightFront) {
-                rightFront = null
-            }
-        }
-    
+
+        let pawnTakePlaces = this.pawnTakes(start,end,ind,true);
+        let leftFront = pawnTakePlaces[0];
+        let rightFront = pawnTakePlaces[1];
+
         if (!front && !leftFront && !rightFront && !front2) {
             console.log('Invalid pawn move')
     
             return false
         }
+
+        if(front2 != null) {
+            this.enPassant = true
+        }
+        else {
+            this.enPassant = false
+        }
+    
+        if(end.name[1] === '8') {
+            this.pawnBackRank(start, true)
+        }
+        return true
+    }
+
+    validBlackPawnMove(start, end) {
+        console.log('Trying pawn move')
+        if(!this.checkStartAndEnd(start,end)) {
+            console.log('Invalid pawn move')
+            return false
+        }
+        
+        let ind = this.getColumnIndex(start);
+        let front2 = null;
+    
+        if( start.name[1] === '7' ) {
+            front2 = this.cols[ind] + (parseInt(start.name[1]) - 2)
+        }
+        if (end.name != front2) {
+            front2 = null
+        }
+    
+        let front = this.cols[ind] + (parseInt(start.name[1]) -1);
+        let frontButton = document.querySelector(`button[name="${front}"]`);
+
+        if(frontButton.innerHTML != "") {
+            front2 = null
+            front = null
+        }
+
+        if (end.name != front) {
+            front = null
+        }
+
+        let pawnTakePlaces = this.pawnTakes(start,end,ind,false);
+        let leftFront = pawnTakePlaces[0];
+        let rightFront = pawnTakePlaces[1];
+    
+        if (!front && !leftFront && !rightFront && !front2) {
+            console.log('Invalid pawn move')
+            return false
+        }
+
+        if(front2 != null) {
+            this.enPassant = true
+        }
+        else {
+            this.enPassant = false
+        }
+
+        if(end.name[1] === '1') {
+            this.pawnBackRank(start)
+        }
     
         return true
     }
 
-    validWhiteRookMove(start, end) {
+    pawnTakes(start,end,ind,whitePawn = false) {
+        let leftFront = null;
+        let rightFront = null;
+        let pieces = null;
+        let leftCol = null;
+        let rightCol = null;
+        let rowChange = 0;
+        let leftChange = 0;
+        let rightChange = 0;
+        if(whitePawn) {
+            pieces = this.blackPieces
+            leftCol = 'a'
+            rightCol = 'h'
+            rowChange = 1
+            leftChange = -1
+            rightChange = 1
+        }
+        else {
+            pieces = this.whitePieces
+            leftCol = 'h'
+            rightCol = 'a'
+            rowChange = -1
+            leftChange = 1
+            rightChange = -1
+        }
+        if (start.name[0] != leftCol) {
+            if (Object.values(pieces).includes(end.innerHTML)) {
+                leftFront = this.cols[ind + leftChange] + (parseInt(start.name[1]) + rowChange)
+            }
+            if (end.name != leftFront) {
+                leftFront = null
+            }
+        }
+        if (start.name[0] != rightCol) {
+            if (Object.values(pieces).includes(end.innerHTML)) {
+                rightFront = this.cols[ind + rightChange] + (parseInt(start.name[1]) + rowChange)
+            }
+            if (end.name != rightFront) {
+                rightFront = null
+            }
+        }
+        return [leftFront, rightFront]
+    }
+
+    pawnBackRank(start, whitePawn = false) {
+        let pieces = this.blackPieces;
+
+        if(whitePawn) {
+            pieces = this.whitePieces
+        }
+
+        let noValidInput = true
+        while(noValidInput) {
+            let newPiece = prompt("Choose: Queen, Rook, Bishop, Knight");
+            if (newPiece.toLowerCase() == 'queen') {
+                start.innerHTML = pieces['queen']
+                noValidInput = false
+            }
+            else if (newPiece.toLowerCase() == 'rook') {
+                start.innerHTML = pieces['rook']
+                noValidInput = false
+            }
+            else if (newPiece.toLowerCase() == 'bishop') {
+                start.innerHTML = pieces['bishop']
+                noValidInput = false
+            }
+            else if (newPiece.toLowerCase() == 'knight') {
+                start.innerHTML = pieces['knight']
+                noValidInput = false
+            }
+            else {
+                'Please write one of 4 options: Queen, Rook, Bishop, Knight'
+            }
+        }
+    }
+
+    validRookMove(start, end) {
         console.log('Trying rook move')
-        if(start.name === end.name) {
+        if(!this.checkStartAndEnd(start,end)) {
+            return false
+        }
+
+        if(!this.validLinearMove(start,end)) {
             console.log('Invalid rook move')
+            return false
+        }
+        return true
+    }
+
+    validBishopMove(start, end) {
+        console.log('Trying bishop move')
+        if(!this.checkStartAndEnd(start,end)) {
+            console.log('Invalid bishop move')
+            return false
+        }
+
+        if(!this.validDiagonalMove(start,end)) {
+            console.log('Invalid bishop move')
+            return false
+        }
+        return true
+    }
+
+    validKnightMove(start, end) {
+        console.log('Trying knight move')
+        if(!this.checkStartAndEnd(start,end)) {
+            return false
+        }
+
+        let startColInd = this.getColumnIndex(start);
+        let endColInd = this.getColumnIndex(end);
+        let startRowInd = start.name[1] - 1;
+        let endRowInd = end.name[1] - 1;
+
+        //Col move 1
+        if(Math.abs(startColInd-endColInd) === 1) {
+            if(Math.abs(startRowInd-endRowInd) != 2) {
+                console.log('Invalid knight move')
+                return false
+            }
+            return true
+        }
+        //Col move 2
+        else if (Math.abs(startColInd-endColInd) === 2) {
+            if(Math.abs(startRowInd-endRowInd) != 1) {
+                console.log('Invalid knight move')
+                return false
+            }
+            return true
+        }
+        else {
+            console.log('Invalid knight move')
+            return false
+        }
+    }
+
+    validKingMove(start, end) {
+        console.log('Trying king move')
+        if(!this.checkStartAndEnd(start,end)) {
+            return false
+        }
+
+        let startColInd = this.getColumnIndex(start);
+        let endColInd = this.getColumnIndex(end);
+        let startRowInd = start.name[1] - 1;
+        let endRowInd = end.name[1] - 1;
+
+        if(Math.abs(startRowInd-endRowInd) > 1) {
+            console.log('Invalid king move')
+            return false
+        }
+        else if(Math.abs(startColInd-endColInd) > 1) {
+            console.log('Invalid king move')
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
+    validQueenMove(start, end) {
+        console.log('Trying queen move')
+        if(!this.checkStartAndEnd(start,end)) {
+            console.log('Invalid queen move')
+            return false
+        }
+
+        if(!this.validDiagonalMove(start,end) && !this.validLinearMove(start,end)) {
+            console.log('Invalid queen move')
+            return false
+        }
+
+        return true
+    }
+
+    checkEmpty(boardSpace) {
+        if(Object.values(this.blackPieces).includes(boardSpace.innerHTML)) {
+            console.log('Invalid move: piece in the way')
+            return false
+        }
+        else if(Object.values(this.whitePieces).includes(boardSpace.innerHTML)) {
+            console.log('Invalid move: piece in the way')
+            return false
+        }
+        return true
+    }
+
+    checkStartAndEnd(start, end) {
+        let piecesToCheck = null;
+        if(Object.values(this.whitePieces).includes(start.innerHTML)) {
+            piecesToCheck = this.whitePieces
+        }
+        else {
+            piecesToCheck = this.blackPieces
+        }
+
+        if(start.name === end.name) {
+            console.log('Invalid move: start = end')
             return false
         } 
-        else if(Object.values(this.whitePieces).includes(end.innerHTML)) {
-            console.log('Invalid rook move')
+        else if(Object.values(piecesToCheck).includes(end.innerHTML)) {
+            console.log('Invalid move: same color end')
+            return false
+        }
+        return true
+    }
+
+    getColumnIndex(boardSpace) {
+        let index = 0;
+        for(let letter of this.cols ) {
+            if ( letter === boardSpace.name[0] ) {
+                break;
+            }
+            index += 1
+        }
+        return index
+    }
+
+    validDiagonalMove(start,end) {
+        let startColInd = this.getColumnIndex(start);
+        let endColInd = this.getColumnIndex(end);
+        let startRowInd = start.name[1] - 1;
+        let endRowInd = end.name[1] - 1;
+
+        let colDiff = Math.abs(startColInd - endColInd);
+        let rowDiff = Math.abs(startRowInd - endRowInd);
+        console.log(colDiff, rowDiff)
+
+        if(colDiff != rowDiff) {
+            console.log('Invalid bishop move: Not diagonal')
             return false
         }
 
-        let startColInd = 0;
-        for( let letter of this.cols ) {
-            if ( letter === start.name[0] ) {
-                break;
+        for(let i = 1; i < colDiff; i++) {
+            let currentSpace = null;
+            // Going right
+            if(startColInd < endColInd) {
+                // Going up
+                if(startRowInd < endRowInd) {
+                    currentSpace = this.cols[startColInd + i] + (parseInt(start.name[1]) + i)
+                }
+                // Going  down
+                else {
+                    currentSpace = this.cols[startColInd + i] + (parseInt(start.name[1]) - i)
+                }
             }
-            startColInd += 1
+            // Going left
+            else {
+                // Going up
+                if(startRowInd < endRowInd) {
+                    currentSpace = this.cols[startColInd - i] + (parseInt(start.name[1]) + i)
+                }
+                // Going  down
+                else {
+                    currentSpace = this.cols[startColInd - i] + (parseInt(start.name[1]) - i)
+                }
+            }
+            
+            let checkSpace = document.querySelector(`button[name="${currentSpace}"]`);
+            console.log(checkSpace)
+            if( !this.checkEmpty(checkSpace)) {
+                return false
+            }
         }
+        return true
+    }
 
-        let endColInd = 0;
-        for( let letter of this.cols ) {
-            if ( letter === end.name[0] ) {
-                break;
-            }
-            endColInd += 1
-        }
+    validLinearMove(start, end) {
+        let startColInd = this.getColumnIndex(start);
+        let endColInd = this.getColumnIndex(end);
 
         if (start.name[0] === end.name[0]) {
             let startRow = start.name[1]-1;
             let endRow = end.name[1]-1;
             if(startRow > endRow) {
-                console.log('Trying more 1')
                 for( let checkInd = endRow + 1; checkInd < startRow; checkInd++) {
                     let currentSpace = start.name[0] + (checkInd+1);
                     let checkSpace = document.querySelector(`button[name="${currentSpace}"]`)
@@ -211,21 +503,8 @@ class chessBoard {
             }
         }
         else {
-            console.log('Invalid rook move')
             return false
         }
-    }
-
-    checkEmpty(boardSpace) {
-        if(Object.values(this.blackPieces).includes(boardSpace.innerHTML)) {
-            console.log('Invalid rook move')
-            return false
-        }
-        else if(Object.values(this.whitePieces).includes(boardSpace.innerHTML)) {
-            console.log('Invalid rook move')
-            return false
-        }
-        return true
     }
 }
 
